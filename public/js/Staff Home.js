1960f5994aaefae3a475b438a089064a82180f70
Lokesh <lokesh3299@gmail.com>
2022-11-22 13:11:06 +0530

    adding quiz pages

--- public/js/Staff Home.js
+++ public/js/Staff Home.js
@@ -1,36 +1,8 @@
-// window.onload = function (e) {
-//     var tbody = document.getElementById('tbody');
-//     fetch(`http://localhost:3000/showQues`)
-//         .then(res => res.json())
-//         .then(data => {
-//             console.log(data);
-//             for (let i = 0; i < data.items.length; i++) {
-//                 var tr = document.createElement("tr");
-//                 var td1 = document.createElement("td");
-//                 var txt1 = document.createTextNode(data.items[i]['firstName']);
-//                 td1.appendChild(txt1);
-//                 var td2 = document.createElement("td");
-//                 var txt2 = document.createTextNode(data.items[i]['lastName']);
-//                 td2.appendChild(txt2);
-//                 var td3 = document.createElement("td");
-//                 var txt3 = document.createTextNode(data.items[i]['house']);
-//                 td3.appendChild(txt3);
-//                 tr.appendChild(td1);
-//                 tr.appendChild(td2);
-//                 tr.appendChild(td3);
-//                 tbody.appendChild(tr);
-//             }
-//         }).catch((e) => console.log(e))
-// }
-const addCSV = (e) => {
-    e.preventDefault();
-    const input = document.getElementById('fileinput');
-    console.log(input.files[0]);
-    var formData = new FormData()
-    formData.append('file', input.files[0]);
-    fetch('http://localhost:3000/ques', {
-        method: 'POST',
-        body: formData
-    })
+let popup = document.getElementById("popup");
+
+function openPopup(){
+    popup.classList.add("open-popup");
 }
-document.getElementById('upload_form').addEventListener('submit', addCSV)
+function closePopup(){
+    popup.classList.remove("open-popup");
+}
